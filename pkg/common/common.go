c9e7d921cfb9beb5bc8f1d9f7da1acd7a9ddf783
menglc <75912001@qq.com>
2023-11-20 03:18:28 +0800

    测试

---
+++ pkg/common/common.go
@@ -0,0 +1,13 @@
+package common
+
+const ProjectName = "social"
+
+const EtcdWatchMsgTypeService string = "service"
+const EtcdWatchMsgTypeCommand string = "command"
+
+const EtcdTtlSecondDefault int64 = 33 //默认TTL时间 秒
+
+// BenchJsonAvailableLoadMaxDefault 可用负载 最大 默认值
+const BenchJsonAvailableLoadMaxDefault = 100000000
+
+const LogAbsPath string = "/data/nd/social/log"
