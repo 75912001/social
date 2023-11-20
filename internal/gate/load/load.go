db2f926625252800a2f78222e380d9cc4e60cb45
menglc <75912001@qq.com>
2023-11-21 00:42:00 +0800

    测试

--- internal/gate/load/load.go
+++ internal/gate/load/load.go
@@ -1,6 +1,8 @@
 package load
 
+import "social/pkg/common"
+
 // AvailableLoad 可用负载
 func AvailableLoad() uint32 {
-	return 10000 // todo menglingchao
+	return common.BenchJsonAvailableLoadMaxDefault // todo menglingchao
 }
