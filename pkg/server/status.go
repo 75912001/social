7a10db437ef93426067ad51ff64a2999b42092b2
menglc <75912001@qq.com>
2023-11-22 21:29:31 +0800

    测试

---
+++ pkg/server/status.go
@@ -0,0 +1,21 @@
+package server
+
+import "sync/atomic"
+
+const StatusRunning uint32 = 0  // 服务状态：运行中
+const StatusStopping uint32 = 1 // 服务状态：关闭中
+
+// IsStopping 服务是否关闭中
+func (p *mgr) IsStopping() bool {
+	return atomic.LoadUint32(&p.Status) == StatusStopping
+}
+
+// IsRunning 服务是否运行中
+func (p *mgr) IsRunning() bool {
+	return atomic.LoadUint32(&p.Status) == StatusRunning
+}
+
+// SetStopping 设置为关闭中
+func (p *mgr) SetStopping() {
+	atomic.StoreUint32(&p.Status, StatusStopping)
+}
