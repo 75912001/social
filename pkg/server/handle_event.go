1800cbf9c5408aa1ffff7097ca60e04e100eb275
menglc <75912001@qq.com>
2023-11-22 22:32:55 +0800

    测试

--- pkg/server/handle_event.go
+++ pkg/server/handle_event.go
@@ -8,27 +8,24 @@ import (
 	"time"
 )
 
-type OnDefaultHandler func(v interface{}) error
-
-// HandleEvent todo [重要] issue 在处理 event 时候, 向 eventChan 中插入 事件，注意超出eventChan的上限会阻塞.
-func HandleEvent(eventChan chan interface{}, onDefaultFunc OnDefaultHandler) {
-	// 在消费eventChan时可能会往eventChan中写入事件，所以关闭服务时不能close eventChan（造成写入阻塞），通过定时检查eventChan大小来关闭
+// HandleBus todo [重要] issue 在处理 event 时候, 向 eventChan 中插入 事件，注意超出eventChan的上限会阻塞.
+func (p *mgr) HandleBus() {
+	// 在消费eventChan时可能会往eventChan中写入事件,所以关闭服务时不能close eventChan(造成写入阻塞),通过定时检查eventChan大小来关闭
 	for {
 		select {
 		case <-GBusChannelCheckChan:
 			xrlog.GetInstance().Warn("receive GBusChannelCheckChan")
-			if 0 == len(eventChan) && IsServerStopping() {
+			if 0 == len(p.BusChannel) && p.IsStopping() {
 				xrlog.GetInstance().Warn("server is stopping, stop consume GEventChan with length 0")
 				return
 			} else {
-				xrlog.GetInstance().Warnf("server is stopping, waiting for consume GEventChan with length:%d", len(eventChan))
+				xrlog.GetInstance().Warnf("server is stopping, waiting for consume GEventChan with length:%d", len(p.BusChannel))
 			}
-		case v := <-eventChan:
+		case v := <-p.BusChannel:
 			//TODO [*] 应拿尽拿...
-			GMgr.TimeMgr.Update()
+			p.TimeMgr.Update()
 			var err error
 			switch t := v.(type) {
-			//timer
 			case *xrtimer.Second:
 				if t.IsValid() {
 					t.Function(t.Arg)
@@ -40,19 +37,17 @@ func HandleEvent(eventChan chan interface{}, onDefaultFunc OnDefaultHandler) {
 			case *xretcd.KV:
 				err = xretcd.GetInstance().Handler(t.Key, t.Value)
 			default:
-				if onDefaultFunc == nil {
+				if p.Opt.DefaultHandler == nil {
 					xrlog.GetInstance().Fatalf("non-existent event:%v %v", v, t)
 				} else {
-					err = onDefaultFunc(v)
+					err = p.Opt.DefaultHandler(v)
 				}
 			}
-
-			if err != nil { // todo 将日志放在每个 case 中,实例化输出对应的数据...
+			if err != nil {
 				xrlog.PrintErr(v, err)
 			}
-
 			if xrutil.IsDebug() {
-				dt := time.Now().Sub(GMgr.TimeMgr.Time).Milliseconds()
+				dt := time.Now().Sub(p.TimeMgr.Time).Milliseconds()
 				if dt > 50 {
 					xrlog.GetInstance().Warnf("cost time50: %v Millisecond with event type:%T", dt, v)
 				} else if dt > 20 {
