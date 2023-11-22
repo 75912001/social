38e1cb9d5cae6169cdb6982408cc58952ab3df62
menglc <75912001@qq.com>
2023-11-23 02:24:41 +0800

    测试

--- internal/gate/handler/timer.go
+++ internal/gate/handler/timer.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"social/internal/gate/load"
+	"social/pkg/bench"
 	xretcd "social/pkg/lib/etcd"
 	xrlog "social/pkg/lib/log"
 	xrtimer "social/pkg/lib/timer"
@@ -16,9 +17,9 @@ type ServerTimer struct {
 }
 
 func (p *ServerTimer) Start() {
-	newDayBeginSec := xrutil.DayBeginSec(&server.GMgr.TimeMgr.Time) + xrutil.OneDaySecond
-	p.newDay = server.GMgr.Timer.AddSecond(newDayTimeOut, p, newDayBeginSec)
-	p.each5min = server.GMgr.Timer.AddSecond(each5minTimeOut, p, server.GMgr.TimeMgr.Second+xrutil.OneMinuteSecond*5)
+	newDayBeginSec := xrutil.DayBeginSec(&server.GetInstance().TimeMgr.Time) + xrutil.OneDaySecond
+	p.newDay = xrtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
+	p.each5min = xrtimer.GetInstance().AddSecond(each5minTimeOut, p, server.GetInstance().TimeMgr.Second+xrutil.OneMinuteSecond*5)
 }
 
 func (p *ServerTimer) Stop() {
@@ -29,22 +30,22 @@ func (p *ServerTimer) Stop() {
 // 新的一天
 func newDayTimeOut(arg interface{}) {
 	p := arg.(*ServerTimer)
-	newDayBeginSec := xrutil.DayBeginSec(&server.GMgr.TimeMgr.Time) + xrutil.OneDaySecond
-	p.newDay = server.GMgr.Timer.AddSecond(newDayTimeOut, p, newDayBeginSec)
+	newDayBeginSec := xrutil.DayBeginSec(&server.GetInstance().TimeMgr.Time) + xrutil.OneDaySecond
+	p.newDay = xrtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
 	//do...
 }
 
 // 每5分钟
 func each5minTimeOut(arg interface{}) {
 	p := arg.(*ServerTimer)
-	p.each5min = server.GMgr.Timer.AddSecond(each5minTimeOut, p, server.GMgr.TimeMgr.Second+xrutil.OneMinuteSecond*5)
+	p.each5min = xrtimer.GetInstance().AddSecond(each5minTimeOut, p, server.GetInstance().TimeMgr.Second+xrutil.OneMinuteSecond*5)
 	{ //更新load
-		server.GMgr.Bench.Json.Etcd.Value.AvailableLoad = load.AvailableLoad()
-		v, err := json.Marshal(server.GMgr.Bench.Json.Etcd.Value)
+		bench.GetInstance().Etcd.Value.AvailableLoad = load.AvailableLoad()
+		v, err := json.Marshal(bench.GetInstance().Etcd.Value)
 		if err != nil {
 			xrlog.GetInstance().Warnf("OnEventEtcd value json Marshal err:%v", err)
 			return
 		}
-		_, _ = xretcd.GetInstance().PutWithLease(server.GMgr.Bench.Json.Etcd.Key, string(v))
+		_, _ = xretcd.GetInstance().PutWithLease(bench.GetInstance().Etcd.Key, string(v))
 	}
 }
