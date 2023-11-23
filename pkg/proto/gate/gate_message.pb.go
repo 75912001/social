7694620dc739adcaf7ab33bb152915f8915e2ec0
menglc <75912001@qq.com>
2023-11-23 22:50:05 +0800

    测试

--- pkg/proto/gate/gate_message.pb.go
+++ pkg/proto/gate/gate_message.pb.go
@@ -21,6 +21,7 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+// c <-> gate 之间通过的用户消息[0x1000,0x1fff]
 type RegisterReq struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
