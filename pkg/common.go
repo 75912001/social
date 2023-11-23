1528a3c00fe5f90e32c1b79074fd405ab906fea9
menglc <75912001@qq.com>
2023-11-24 01:57:13 +0800

    测试

--- pkg/common.go
+++ pkg/common.go
@@ -4,3 +4,9 @@ const EtcdWatchMsgTypeService string = "service"
 const EtcdWatchMsgTypeCommand string = "command"
 
 const EtcdTtlSecondDefault int64 = 33 //默认TTL时间 秒
+
+var (
+	GZoneID      uint32 // 区域ID
+	GServiceName string // 服务
+	GServiceID   uint32 // 服务ID
+)
