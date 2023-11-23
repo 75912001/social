6eec9f81650f94b024e33877ce858cf6f8afb134
menglc <75912001@qq.com>
2023-11-24 01:12:17 +0800

    测试

--- pkg/msg/packet.go
+++ pkg/msg/packet.go
@@ -10,7 +10,7 @@ import (
 type Packet struct{}
 
 // Marshal 序列化 数据包
-func (p *Packet) Marshal(unserializedPacket *xrpb.UnserializedPacket) ([]byte, error) {
+func Marshal(unserializedPacket *xrpb.UnserializedPacket) ([]byte, error) {
 	headerBuf := unserializedPacket.Header.Pack()
 
 	var messageBuf []byte
@@ -30,7 +30,7 @@ func (p *Packet) Marshal(unserializedPacket *xrpb.UnserializedPacket) ([]byte, e
 // Unmarshal 反序列化
 //
 //	data:完整包数据 包头+包体
-func (p *Packet) Unmarshal(data []byte) (unserializedPacket *xrpb.UnserializedPacket, err error) {
+func Unmarshal(data []byte) (unserializedPacket *xrpb.UnserializedPacket, err error) {
 	var msg proto.Message
 	if err = proto.Unmarshal(data[GProtoHeadLength:], msg); nil != err {
 		return nil, errors.WithMessagef(err, "%v data:%v", xrutil.GetCodeLocation(1).String(), data)
