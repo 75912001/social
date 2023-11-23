3ef11413e0dce1381ab775bee916da71024431d2
menglc <75912001@qq.com>
2023-11-24 02:00:16 +0800

    测试

--- cmd/gate/main.go
+++ cmd/gate/main.go
@@ -5,16 +5,18 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
-	"log"
 	"net"
 	"os"
 	"os/signal"
 	"runtime"
-	api_gate "social/api/gate"
+	"runtime/debug"
+	apigate "social/api/gate"
 	"social/internal/gate/handler"
 	"social/internal/gate/stop"
+	"social/pkg"
+	"social/pkg/bench"
 	"social/pkg/common"
-	"social/pkg/etcd"
+	xrconstant "social/pkg/lib/constant"
 	xrlog "social/pkg/lib/log"
 	xrutil "social/pkg/lib/util"
 	"social/pkg/proto/gate"
@@ -50,17 +52,79 @@ func (p *Server) Start() (err error) {
 	err = server.GetInstance().PostInit(context.TODO(),
 		server.NewOptions().
 			SetEtcdHandler(handler.OnEventEtcd).
-			SetEtcdWatchServicePrefix(fmt.Sprintf("/%v/%v/", common.ProjectName, etcd.WatchMsgTypeService)).
+			SetEtcdWatchServicePrefix(fmt.Sprintf("/%v/%v/", common.ProjectName, pkg.EtcdWatchMsgTypeService)).
 			SetEtcdWatchCommandPrefix(fmt.Sprintf("/%v/%v/%v/%v/",
-				common.ProjectName, etcd.WatchMsgTypeCommand,
-				server.GetInstance().ZoneID,
-				server.GetInstance().ServiceName)),
+				common.ProjectName, pkg.EtcdWatchMsgTypeCommand,
+				pkg.GZoneID,
+				pkg.GServiceName)),
 	)
 	if err != nil {
 		return errors.WithMessage(err, xrutil.GetCodeLocation(1).String())
 	}
 
 	runtime.GC()
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//
+	//向客户端发送通知消息
+	//go func() {
+	//	time.Sleep(time.Second * 60)
+	//	// 模拟异步发送通知给特定客户端
+	//	notification := &social_service.CommonResponse{
+	//		Response: &social_service.CommonResponse_LogoutResponse{
+	//			LogoutResponse: &social_service.LogoutResponse{
+	//				Field2: 1,
+	//			},
+	//		},
+	//	}
+	//	clients.Range(func(key, value interface{}) bool {
+	//		fmt.Printf("Key: %v, Value: %v\n", key, value)
+	//		if err := value.(social_service.SocialService_BidirectionalStreamingMethodServer).Send(notification); err != nil {
+	//			log.Printf("Error sending no")
+	//		}
+	//		return true // 返回 true 继续遍历，返回 false 停止遍历
+	//
+	//
+	//		LogoutResponse: &pb.ResponseTypeA{
+	//			Field1: "Notification to client",
+	//		},
+	//	}
+	//	clientStream, ok := s.clients.Load(clientID)
+	//	if ok {
+	//		// 发送通知消息
+	//		if err := clientStream.(pb.MyService_MyBidirectionalStreamingMethodServer).Send(notification); err != nil {
+	//			log.Printf("Error sending notification: %v", err)
+	//		}
+	//	}
+	//}()
+
+	go func() { //启动grpc服务
+		defer func() {
+			if xrutil.IsRelease() {
+				if err := recover(); err != nil {
+					xrlog.GetInstance().Fatalf(xrconstant.GoroutinePanic, err, debug.Stack())
+				}
+			}
+			//todo menglingchao 关闭grpc服务...
+			// p.waitGroupOutPut.Done()
+			xrlog.GetInstance().Fatalf(xrconstant.GoroutineDone)
+		}()
+		addr := fmt.Sprintf("%v:%v", bench.GetInstance().Server.IP, bench.GetInstance().Server.Port)
+		listen, err := net.Listen("tcp", addr)
+		if err != nil {
+			xrlog.GetInstance().Fatalf("Failed to listen: %v", err)
+		}
+
+		server := grpc.NewServer()
+		gate.RegisterServiceServer(server, &apigate.Server{})
+
+		xrlog.GetInstance().Tracef("Server is running on %v", addr)
+		if err = server.Serve(listen); err != nil {
+			xrlog.GetInstance().Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// 退出服务
 	sigChan := make(chan os.Signal, 1)
@@ -77,51 +141,4 @@ func (p *Server) Start() (err error) {
 	_ = server.GetInstance().Stop()
 
 	return nil
-}
-
-func main() {
-	// 向客户端发送通知消息
-	//go func() {
-	//	//time.Sleep(time.Second * 60)
-	//	//// 模拟异步发送通知给特定客户端
-	//	//notification := &social_service.CommonResponse{
-	//	//	Response: &social_service.CommonResponse_LogoutResponse{
-	//	//		LogoutResponse: &social_service.LogoutResponse{
-	//	//			Field2: 1,
-	//	//		},
-	//	//	},
-	//	//}
-	//	//clients.Range(func(key, value interface{}) bool {
-	//	//	fmt.Printf("Key: %v, Value: %v\n", key, value)
-	//	//	if err := value.(social_service.SocialService_BidirectionalStreamingMethodServer).Send(notification); err != nil {
-	//	//		log.Printf("Error sending no")
-	//	//	}
-	//	//	return true // 返回 true 继续遍历，返回 false 停止遍历
-	//	//})
-	//}()
-	////		LogoutResponse: &pb.ResponseTypeA{
-	////			Field1: "Notification to client",
-	////		},
-	////	}
-	////	clientStream, ok := s.clients.Load(clientID)
-	////	if ok {
-	////		// 发送通知消息
-	////		if err := clientStream.(pb.MyService_MyBidirectionalStreamingMethodServer).Send(notification); err != nil {
-	////			log.Printf("Error sending notification: %v", err)
-	////		}
-	////	}
-	////}()
-
-	listen, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-
-	server := grpc.NewServer()
-	gate.RegisterServiceServer(server, &api_gate.Server{})
-
-	fmt.Println("Server is running on :50051")
-	if err := server.Serve(listen); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
 }
