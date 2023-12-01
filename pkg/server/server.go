1db49a349fdaf572397cf12346efe60b9b2348be
menglc <75912001@qq.com>
2023-12-01 22:14:51 +0800

    测试

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,6 +5,7 @@ import (
 )
 
 type IServer interface {
+	Init(ctx context.Context, opts ...*options) error
 	Start(ctx context.Context) error
 	Run(ctx context.Context) error
 	PreStop(ctx context.Context) error //处理服务停止前的逻辑
