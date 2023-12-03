4f2f1790a8f74bf05b4d8c4e3d4520e522dd1c19
menglc <75912001@qq.com>
2023-12-03 19:40:16 +0800

    测试

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,8 +1,4 @@
 package common
 
 const ProjectName = "social"
-
-// BenchJsonAvailableLoadMaxDefault 可用负载 最大 默认值
-const BenchJsonAvailableLoadMaxDefault = 100000000
-
 const LogAbsPath string = "/data/nd/social/log"
