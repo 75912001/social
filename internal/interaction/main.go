f8a3b07d1846d600147c82aa5203da36ab6f04f8
menglc <75912001@qq.com>
2023-12-03 20:01:22 +0800

    测试

--- internal/interaction/main.go
+++ internal/interaction/main.go
@@ -1,8 +1,12 @@
 package interaction
 
-import "context"
+import (
+	"context"
+	pkgserver "social/pkg/server"
+)
 
 type Server struct {
+	*pkgserver.Normal
 }
 
 func (p *Server) Stop(ctx context.Context) (err error) {
