a13a10f2b09d0b5f34f55c31849b6119ac6dc33e
menglc <75912001@qq.com>
2023-12-04 00:02:20 +0800

    测试

--- internal/gate/handler/etcd.go
+++ internal/gate/handler/etcd.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"social/pkg"
-	"social/pkg/etcd"
-	xrconstant "social/pkg/lib/constant"
-	xrlog "social/pkg/lib/log"
-	xrutil "social/pkg/lib/util"
-	"social/pkg/server"
+	pkgcommon "social/pkg/common"
+	pkgetcd "social/pkg/etcd"
+	libconstant "social/pkg/lib/constant"
+	liblog "social/pkg/lib/log"
+	libutil "social/pkg/lib/util"
+	pkgserver "social/pkg/server"
 	"strconv"
 )
 
@@ -14,49 +14,52 @@ import (
 // e.g.:/projectName/service/${zoneID}/${serviceName}/${serviceID}
 // e.g.:/projectName/command/${zoneID}/${serviceName}/${serviceID}
 func OnEventEtcd(key string, value string) error {
-	xrlog.GetInstance().Infof("%v key:%v, value:%v", xrconstant.Etcd, key, value)
+	liblog.GetInstance().Infof("%v key:%v, value:%v", libconstant.Etcd, key, value)
 
 	var zoneIDU32 uint32
 	var serviceIDU32 uint32
-	msgType, zoneID, serviceName, serviceID := etcd.Parse(key)
+	msgType, zoneID, serviceName, serviceID := pkgetcd.Parse(key)
 
 	if zoneIDU64, err := strconv.ParseUint(zoneID, 10, 32); err != nil {
-		xrlog.GetInstance().Fatal(xrconstant.Etcd, key, value, err)
+		liblog.GetInstance().Errorf(libconstant.Etcd, key, value, err)
 		return nil
 	} else {
 		zoneIDU32 = uint32(zoneIDU64)
 	}
 
 	switch msgType {
-	case pkg.EtcdWatchMsgTypeCommand:
+	case pkgcommon.EtcdWatchMsgTypeCommand:
 		//todo menglingchao
-	case pkg.EtcdWatchMsgTypeService:
+	case pkgcommon.EtcdWatchMsgTypeService:
 		if serviceIDU64, err := strconv.ParseUint(serviceID, 10, 64); err != nil {
-			xrlog.GetInstance().Fatal(xrconstant.Etcd, key, value, serviceID, err)
+			liblog.GetInstance().Fatal(libconstant.Etcd, key, value, serviceID, err)
 			return nil
 		} else {
 			serviceIDU32 = uint32(serviceIDU64)
 		}
 		switch serviceName {
 		// 收到其它 服务 启动、关闭 的信息
-		case server.NameGate: //网关
-		case server.NameFriend: //好友
+		case pkgserver.NameGate: //网关
+		case pkgserver.NameFriend: //好友
 			if 0 == len(value) {
-				xrlog.GetInstance().Warnf("%s delete service with key:%s, value empty", xrconstant.Etcd, key)
+				liblog.GetInstance().Warnf("%s delete service with key:%s, value empty", libconstant.Etcd, key)
 				//todo menglingchao  将该服务从所在区域中移除
 				return nil
+			} else {
+				//todo menglingchao
+				//查找,添加,链接
 			}
-			xrlog.GetInstance().Warnf("%s add service zone_id:%d service_id:%d with key:%s",
-				xrconstant.Etcd, zoneIDU32, serviceIDU32, key)
+			liblog.GetInstance().Warnf("%s add service zone_id:%d service_id:%d with key:%s",
+				libconstant.Etcd, zoneIDU32, serviceIDU32, key)
 			//todo menglingchao 添加服务
-		case server.NameInteraction: //交互
-		case server.NameNotification: //通知
-		case server.NameBlog: //博客
-		case server.NameRecommendation: //推荐
-		case server.NameCleansing: //清洗
-		case server.NameRobot: //清洗
+		case pkgserver.NameInteraction: //交互
+		case pkgserver.NameNotification: //通知
+		case pkgserver.NameBlog: //博客
+		case pkgserver.NameRecommendation: //推荐
+		case pkgserver.NameCleansing: //清洗
+		case pkgserver.NameRobot: //清洗
 		default:
-			xrlog.GetInstance().Errorf("%v %v %v", key, value, xrutil.GetCodeLocation(1))
+			liblog.GetInstance().Errorf("%v %v %v", key, value, libutil.GetCodeLocation(1))
 		}
 	default:
 	}
--- internal/gate/handler/timer.go
+++ internal/gate/handler/timer.go
@@ -2,50 +2,54 @@ package handler
 
 import (
 	"encoding/json"
-	"social/internal/gate/load"
-	"social/pkg/bench"
-	xretcd "social/pkg/lib/etcd"
-	xrlog "social/pkg/lib/log"
-	xrtimer "social/pkg/lib/timer"
-	xrutil "social/pkg/lib/util"
-	"social/pkg/server"
+	gateload "social/internal/gate/load"
+	pkgbench "social/pkg/bench"
+	libetcd "social/pkg/lib/etcd"
+	liblog "social/pkg/lib/log"
+	libutil "social/pkg/lib/time"
+	libtimer "social/pkg/lib/timer"
+	pkgserver "social/pkg/server"
 )
 
-type ServerTimer struct {
-	newDay   *xrtimer.Second
-	each5min *xrtimer.Second
+func NewTimer() *Timer {
+	return new(Timer)
 }
 
-func (p *ServerTimer) Start() {
-	newDayBeginSec := xrutil.DayBeginSec(&server.GetInstance().TimeMgr.Time) + xrutil.OneDaySecond
-	p.newDay = xrtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
-	p.each5min = xrtimer.GetInstance().AddSecond(each5minTimeOut, p, server.GetInstance().TimeMgr.Second+xrutil.OneMinuteSecond*5)
+type Timer struct {
+	newDay   *libtimer.Second
+	each5min *libtimer.Second
 }
 
-func (p *ServerTimer) Stop() {
-	xrtimer.DelSecond(p.newDay)
-	xrtimer.DelSecond(p.each5min)
+func (p *Timer) Start() {
+	newDayBeginSec := libutil.DayBeginSec(&pkgserver.GetInstance().TimeMgr.Time) + libutil.OneDaySecond
+	p.newDay = libtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
+	p.each5min = libtimer.GetInstance().AddSecond(each5minTimeOut, p, pkgserver.GetInstance().TimeMgr.Second+libutil.OneMinuteSecond*5)
+}
+
+func (p *Timer) Stop() {
+	libtimer.DelSecond(p.newDay)
+	libtimer.DelSecond(p.each5min)
 }
 
 // 新的一天
 func newDayTimeOut(arg interface{}) {
-	p := arg.(*ServerTimer)
-	newDayBeginSec := xrutil.DayBeginSec(&server.GetInstance().TimeMgr.Time) + xrutil.OneDaySecond
-	p.newDay = xrtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
+	p := arg.(*Timer)
+	newDayBeginSec := libutil.DayBeginSec(&pkgserver.GetInstance().TimeMgr.Time) + libutil.OneDaySecond
+	p.newDay = libtimer.GetInstance().AddSecond(newDayTimeOut, p, newDayBeginSec)
 	//do...
 }
 
 // 每5分钟
 func each5minTimeOut(arg interface{}) {
-	p := arg.(*ServerTimer)
-	p.each5min = xrtimer.GetInstance().AddSecond(each5minTimeOut, p, server.GetInstance().TimeMgr.Second+xrutil.OneMinuteSecond*5)
+	p := arg.(*Timer)
+	p.each5min = libtimer.GetInstance().AddSecond(each5minTimeOut, p, pkgserver.GetInstance().TimeMgr.Second+libutil.OneMinuteSecond*5)
 	{ //更新load
-		bench.GetInstance().Etcd.Value.AvailableLoad = load.AvailableLoad()
-		v, err := json.Marshal(bench.GetInstance().Etcd.Value)
+		pkgbench.GetInstance().Etcd.Value.AvailableLoad = gateload.AvailableLoad()
+		v, err := json.Marshal(pkgbench.GetInstance().Etcd.Value)
 		if err != nil {
-			xrlog.GetInstance().Warnf("OnEventEtcd value json Marshal err:%v", err)
+			liblog.GetInstance().Warnf("OnEventEtcd value json Marshal err:%v", err)
 			return
 		}
-		_, _ = xretcd.GetInstance().PutWithLease(bench.GetInstance().Etcd.Key, string(v))
+		_, _ = libetcd.GetInstance().PutWithLease(pkgbench.GetInstance().Etcd.Key, string(v))
 	}
 }
