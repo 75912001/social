df6c0dfe93ea11dbd499b2bd442c14f1bcc36937
menglc <75912001@qq.com>
2023-12-08 01:57:25 +0800

    测试

--- lib/etcd/options.go
+++ lib/etcd/options.go
@@ -2,13 +2,15 @@ package etcd
 
 import (
 	"github.com/pkg/errors"
-	liberror "social/pkg/lib/error"
-	libutil "social/pkg/lib/util"
+	liberror "social/lib/error"
+	libutil "social/lib/util"
 	"time"
 )
 
 var (
-	grantLeaseRetryDuration = time.Second * 3 // 授权租约 重试 间隔时长
+	grantLeaseRetryDuration     = time.Second * 3 // 授权租约 重试 间隔时长
+	grantLeaseMaxRetriesDefault = 600             // 授权租约 最大 重试次数 默认值
+	dialTimeoutDefault          = time.Second * 5 //dialTimeout is the timeout for failing to establish a connection. 默认值
 )
 
 // OnFunc 处理数据
@@ -21,13 +23,13 @@ type KV struct {
 }
 
 // NewOptions 新的Options
-func NewOptions() *options {
-	return new(options)
+func NewOptions() *Options {
+	return new(Options)
 }
 
-// options contains options to configure instance. Each option can be set through setter functions. See
+// Options contains Options to configure instance. Each option can be set through setter functions. See
 // documentation for each setter function for an explanation of the option.
-type options struct {
+type Options struct {
 	addrs                []string           // 地址
 	ttl                  *int64             // Time To Live, etcd内部会按照 ttl/3 的时间(最小1秒),保持连接
 	grantLeaseMaxRetries *int               // 授权租约 最大 重试次数 默认:600
@@ -37,99 +39,98 @@ type options struct {
 	outgoingEventChan    chan<- interface{} // 传出 channel
 }
 
-func (p *options) SetAddrs(addrs []string) *options {
+func (p *Options) SetAddrs(addrs []string) *Options {
 	p.addrs = p.addrs[0:0]
 	p.addrs = append(p.addrs, addrs...)
 	return p
 }
 
-func (p *options) SetTTL(ttl int64) *options {
+func (p *Options) SetTTL(ttl int64) *Options {
 	p.ttl = &ttl
 	return p
 }
 
-func (p *options) SetGrantLeaseMaxRetries(retries int) *options {
+func (p *Options) SetGrantLeaseMaxRetries(retries int) *Options {
 	p.grantLeaseMaxRetries = &retries
 	return p
 }
 
-func (p *options) SetKV(kv []KV) *options {
+func (p *Options) SetKV(kv []KV) *Options {
 	p.kvSlice = p.kvSlice[0:0]
 	p.kvSlice = append(p.kvSlice, kv...)
 	return p
 }
 
-func (p *options) SetDialTimeout(dialTimeout time.Duration) *options {
+func (p *Options) SetDialTimeout(dialTimeout time.Duration) *Options {
 	p.dialTimeout = &dialTimeout
 	return p
 }
 
-func (p *options) SetOnFunc(onFunc OnFunc) *options {
+func (p *Options) SetOnFunc(onFunc OnFunc) *Options {
 	p.onFunc = onFunc
 	return p
 }
 
-func (p *options) SetOutgoingEventChan(eventChan chan<- interface{}) *options {
+func (p *Options) SetOutgoingEventChan(eventChan chan<- interface{}) *Options {
 	p.outgoingEventChan = eventChan
 	return p
 }
 
-// mergeOptions combines the given *options into a single *options in a last one wins fashion.
-// The specified options are merged with the existing options, with the specified options taking
+// mergeOptions combines the given *Options into a single *Options in a last one wins fashion.
+// The specified Options are merged with the existing Options, with the specified Options taking
 // precedence.
-func mergeOptions(opts ...*options) *options {
-	no := NewOptions()
+func mergeOptions(opts ...*Options) *Options {
+	newOptions := NewOptions()
 	for _, opt := range opts {
 		if opt == nil {
 			continue
 		}
 		if len(opt.addrs) != 0 {
-			no.SetAddrs(opt.addrs)
+			newOptions.SetAddrs(opt.addrs)
 		}
 		if opt.ttl != nil {
-			no.SetTTL(*opt.ttl)
+			newOptions.SetTTL(*opt.ttl)
 		}
 		if opt.grantLeaseMaxRetries != nil {
-			no.SetGrantLeaseMaxRetries(*opt.grantLeaseMaxRetries)
+			newOptions.SetGrantLeaseMaxRetries(*opt.grantLeaseMaxRetries)
 		}
 		if len(opt.kvSlice) != 0 {
-			no.SetKV(opt.kvSlice)
+			newOptions.SetKV(opt.kvSlice)
 		}
 		if opt.dialTimeout != nil {
-			no.SetDialTimeout(*opt.dialTimeout)
+			newOptions.SetDialTimeout(*opt.dialTimeout)
 		}
 		if opt.onFunc != nil {
-			no.SetOnFunc(opt.onFunc)
+			newOptions.SetOnFunc(opt.onFunc)
 		}
 		if opt.outgoingEventChan != nil {
-			no.SetOutgoingEventChan(opt.outgoingEventChan)
+			newOptions.SetOutgoingEventChan(opt.outgoingEventChan)
 		}
 	}
-	return no
+	return newOptions
 }
 
 // 配置
-func configure(opts *options) error {
-	if len(opts.addrs) == 0 {
+func configure(opt *Options) error {
+	if len(opt.addrs) == 0 {
 		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
 	}
-	if opts.ttl == nil {
+	if opt.ttl == nil {
 		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
 	}
-	if opts.grantLeaseMaxRetries == nil {
-		var v = 600
-		opts.grantLeaseMaxRetries = &v
+	if opt.grantLeaseMaxRetries == nil {
+		opt.grantLeaseMaxRetries = &grantLeaseMaxRetriesDefault
 	}
-	if len(opts.kvSlice) == 0 {
+	if len(opt.kvSlice) == 0 {
 		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
 	}
-	if opts.dialTimeout == nil {
+	if opt.dialTimeout == nil {
+		opt.dialTimeout = &dialTimeoutDefault
+	}
+	if opt.onFunc == nil {
 		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
 	}
-	if opts.onFunc == nil {
-		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
-	}
-	if opts.outgoingEventChan == nil {
+	if opt.outgoingEventChan == nil {
 		return errors.WithMessage(liberror.Param, libutil.GetCodeLocation(1).String())
 	}
 	return nil
