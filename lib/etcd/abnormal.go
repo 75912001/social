c1f47ca8e78c1797a33ec7c51c5672206ccfeee2
menglc <75912001@qq.com>
2023-12-11 03:30:29 +0800

    测试

---
+++ lib/etcd/abnormal.go
@@ -0,0 +1,67 @@
+package etcd
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"runtime/debug"
+	libconsts "social/lib/consts"
+	liblog "social/lib/log"
+	libutil "social/lib/util"
+	"time"
+)
+
+func (p *Mgr) abnormal() {
+	liblog.PrintErr("etcd lease Run abnormal, retrying")
+	go func(ctx context.Context) {
+		defer func() {
+			if libutil.IsRelease() {
+				if err := recover(); err != nil {
+					liblog.PrintErr(libconsts.Retry, libconsts.GoroutinePanic, err, debug.Stack())
+				}
+			}
+			liblog.PrintInfo(libconsts.Retry, libconsts.GoroutineDone)
+		}()
+		if err := p.Stop(); err != nil {
+			liblog.PrintInfo(libconsts.Retry, libconsts.Failure, err)
+			return
+		}
+		if err := p.retryStartAndRun(ctx); err != nil {
+			liblog.PrintErr(libconsts.Retry, libconsts.Failure, err)
+			return
+		}
+	}(context.Background())
+}
+
+// 多次重试 Start 和 Run
+func (p *Mgr) retryStartAndRun(ctx context.Context) error {
+	liblog.PrintfErr("renewing etcd lease, reconfiguring.grantLeaseMaxRetries:%v, grantLeaseIntervalSecond:%v",
+		*p.options.grantLeaseMaxRetries, grantLeaseRetryDuration/time.Second)
+	var failedGrantLeaseAttempts = 0
+	for {
+		if err := p.Start(ctx, p.options); err != nil {
+			failedGrantLeaseAttempts++
+			if *p.options.grantLeaseMaxRetries <= failedGrantLeaseAttempts {
+				return errors.WithMessagef(err, "%v exceeded max attempts to renew etcd lease %v %v",
+					libutil.GetCodeLocation(1), *p.options.grantLeaseMaxRetries, failedGrantLeaseAttempts)
+			}
+			liblog.PrintErr("error granting etcd lease, will retry.", err)
+			time.Sleep(grantLeaseRetryDuration)
+			continue
+		} else {
+		retryKeepAlive:
+			// 续租
+			if err = p.Run(ctx); err != nil {
+				failedGrantLeaseAttempts++
+				if *p.options.grantLeaseMaxRetries <= failedGrantLeaseAttempts {
+					return errors.WithMessagef(err, "%v exceeded max attempts to renew etcd lease %v %v",
+						libutil.GetCodeLocation(1), *p.options.grantLeaseMaxRetries, failedGrantLeaseAttempts)
+				}
+				liblog.PrintErr("error granting etcd lease, will retry.", err)
+				time.Sleep(grantLeaseRetryDuration)
+				goto retryKeepAlive
+			} else {
+				return nil
+			}
+		}
+	}
+}
