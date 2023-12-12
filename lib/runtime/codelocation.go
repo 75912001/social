f44e94a206d2b8c6ea551fe679daa4b8a4b6d69c
menglc <75912001@qq.com>
2023-12-13 01:35:13 +0800

    测试

---
+++ lib/runtime/codelocation.go
@@ -0,0 +1,44 @@
+package runtime
+
+import (
+	"fmt"
+	"runtime"
+	libconstant "social/lib/consts"
+)
+
+// CodeLocation 代码位置
+type CodeLocation struct {
+	FileName string //文件名
+	FuncName string //函数名
+	Line     int    //行数
+}
+
+// Error 错误信息
+func (p *CodeLocation) Error() string {
+	return fmt.Sprintf("file:%v line:%v func:%v", p.FileName, p.Line, p.FuncName)
+}
+
+// String 错误信息
+func (p *CodeLocation) String() string {
+	return p.Error()
+}
+
+// GetCodeLocation 获取代码位置
+//
+//	参数:
+//		skip:The argument skip is the number of stack frames to ascend, with 0 identifying the caller of Caller.
+func GetCodeLocation(skip int) *CodeLocation {
+	c := &CodeLocation{
+		FileName: libconstant.Unknown,
+		FuncName: libconstant.Unknown,
+	}
+
+	pc, fileName, line, ok := runtime.Caller(skip)
+
+	if ok {
+		c.FileName = fileName
+		c.Line = line
+		c.FuncName = runtime.FuncForPC(pc).Name()
+	}
+	return c
+}
